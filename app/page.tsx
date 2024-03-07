69256563808401252178732a28719bb21404bd6b
Siem Debesay <47540832+siemd2@users.noreply.github.com>
2024-03-07 16:58:45 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-import Image from 'next/image'
+import React from 'react';
+import Hero from './components/Hero'
+import five from '../public/assets/LandingPage/LandingPage2.jpg';
+import seven from '../public/assets/LandingPage/LandingPage3.jpg';
+import front from '../public/assets/LandingPage/front.jpg';
+import fivetandem from '../public/assets/500Info/500Info1.jpg';
 
 export default function Home() {
   return (
-    null
-  )
+    <div className='snap-y snap-mandatory h-screen overflow-auto' style={{ scrollBehavior: 'smooth' }}>
+      <Hero
+        title=""
+        subtitle="Explore our range of products"
+        image={front.src} 
+      />
+      <Hero 
+        title="500 Series"
+        subtitle="Explore our range of products"
+        image={five.src} 
+      />
+      <Hero 
+        title="750 Series"
+        subtitle="Explore our range of products"
+        image={seven.src} 
+      />
+      <Hero 
+        title="500 Series - Tandem Drive"
+        subtitle="Explore our range of products"
+        image={fivetandem.src} 
+      />
+
+    </div>
+  );
 }
